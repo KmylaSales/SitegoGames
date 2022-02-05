f045aaabf7ba074cd1680780564b2995fa05d384
kmylas <kammylas2005@gmail.com>
2022-02-04 21:45:56 -0300

    Finalizando Site

---
+++ js/script.js
@@ -0,0 +1,12 @@
+//function mostrarPopup(){
+//    window.alert("Bem-Vindo ao goGames!")
+//}
+
+let email = document.getElementById("campo-email");
+let nome = document.getElementById("campo-nome");
+
+function enviarEmail(){
+    let emailDigitado = email.value;
+    let nomeDigitado = nome.value;
+    console.log(emailDigitado)
+}
