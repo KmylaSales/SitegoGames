d55bd6a36c2d2cd209ee3e61e0b9a557f6210d71
kmylas <kammylas2005@gmail.com>
2022-02-08 22:36:14 -0300

    Testando carousel

--- js/script.js
+++ js/script.js
@@ -1,6 +1,28 @@
+$(document).ready(function(){
+//     $("#barras").click(function(){
+//         $("#menu").toggeClass("#menu-ativo")
+// })
+// })
+    $("#barras").click(function(){
+        if($("#menu").hasClass("#menu-ativo")){
+            $("#menu").removeClass("#menu-ativo")
+        }else{
+            $("#menu").addClass("menu-ativo")
+        }
+    })
+})
+
 //function mostrarPopup(){
 //    window.alert("Bem-Vindo ao goGames!")
 //}
+
+
+
+$('.carousel').slick({
+    infinite: true,
+    slidesToShow: 4,
+    slidesToScroll: 4
+  });
 
 let email = document.getElementById("campo-email");
 let nome = document.getElementById("campo-nome");
