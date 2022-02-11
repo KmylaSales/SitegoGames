f1f89f50437df70cac97178da97efb942a91ccdf
kmylas <kammylas2005@gmail.com>
2022-02-10 23:40:42 -0300

    menu responsivo

--- js/script.js
+++ js/script.js
@@ -1,36 +1,36 @@
 $(document).ready(function(){
-//     $("#barras").click(function(){
-//         $("#menu").toggeClass("#menu-ativo")
-// })
-// })
-    $("#barras").click(function(){
-        if($("#menu").hasClass("#menu-ativo")){
-            $("#menu").removeClass("#menu-ativo")
-        }else{
-            $("#menu").addClass("menu-ativo")
-        }
+    $("#barras").click(function () {
+        $("#menu").toggleClass("menu-ativo")
+
+        // if ($("#menu").hasClass("menu-ativo")) {
+        //     $("#menu").removeClass("menu-ativo")
+        // } else {
+        //     $("#menu").addClass("menu-ativo")
+        // }
     })
 })
+
+
+// let menu = document.getElementById("menu");
+
+// function mostrarMenu() {
+//     if (menu.style.display != "flex") {
+//         menu.style.display = "flex"
+//     } else {
+//         menu.style.display = "none"
+//     }
+// }
+
 
 //function mostrarPopup(){
 //    window.alert("Bem-Vindo ao goGames!")
 //
+
+let menu = document.getElementById("menu");
+
+
     
-var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 4,
-    spaceBetween: 30,
-    slidesPerGroup: 4,
-    loop: true,
-    loopFillGroupWithBlank: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
-  });
+
 
 let email = document.getElementById("campo-email");
 let nome = document.getElementById("campo-nome");
