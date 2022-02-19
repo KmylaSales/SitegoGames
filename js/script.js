cf87a59671f876460a2105727694acc9a15753b9
kmylas <kammylas2005@gmail.com>
2022-02-18 23:40:22 -0300

    ok

--- js/script.js
+++ js/script.js
@@ -15,3 +15,80 @@ function enviarEmail(){
     let nomeDigitado = nome.value;
     console.log(emailDigitado)
 }
+
+
+
+//código da página de login//
+
+const sign_in_senha = document.getElementById('senha');
+const submitButton = document.getElementById('submit-button')
+
+//lista de elementos de validação//
+
+
+const li_maiuculas = document.querySelector('#maiusculas');
+const li_numeros = document.querySelector('#numeros');
+const li_especiais = document.querySelector('#especiais');
+const li_caracteres = document.querySelector('#caracteres');
+
+sign_in_senha.addEventListener('keyup', (event) => {
+    const {value} = sign_in_senha;
+
+    if (value.length >= 8) {
+    li_caracteres.classList.add('active');
+    const icon = li_caracteres.getElementsByTagName('i')[0];
+    icon.classList.remove('fa-circle');
+    icon.classList.add('fa-check');
+    } else {
+    li_caracteres.classList.remove('active');
+    const icon = li_caracteres.getElementsByTagName('i')[0];
+    icon.classList.remove('fa-check');
+    icon.classList.add('fa-circle');
+    }
+
+
+
+
+
+    // if (value.length >= 8) {
+    //     li_caracteres.classList.add('active');
+    //     const icon = li_caracteres.getElementsByTagName('i')[0];
+    //     icon.classList.remove('fa-circle');
+    //     icon.classList.add('fa-check');
+    //     } else {
+    //     li_caracteres.classList.remove('active');
+    //     const icon = li_caracteres.getElementsByTagName('i')[0];
+    //     icon.classList.remove('fa-check');
+    //     icon.classList.add('fa-circle');
+    //     }
+
+
+
+
+    //   if (value.length >= 8) {
+    // li_caracteres.classList.add('active');
+    // const icon = li_caracteres.getElementsByTagName('i')[0];
+    // icon.classList.remove('fa-circle');
+    // icon.classList.add('fa-check');
+    // } else {
+    // li_caracteres.classList.remove('active');
+    // const icon = li_caracteres.getElementsByTagName('i')[0];
+    // icon.classList.remove('fa-check');
+    // icon.classList.add('fa-circle');
+    // }
+
+
+
+
+    // if (value.length >= 8) {
+    //     li_caracteres.classList.add('active');
+    //     const icon = li_caracteres.getElementsByTagName('i')[0];
+    //     icon.classList.remove('fa-circle');
+    //     icon.classList.add('fa-check');
+    //     } else {
+    //     li_caracteres.classList.remove('active');
+    //     const icon = li_caracteres.getElementsByTagName('i')[0];
+    //     icon.classList.remove('fa-check');
+    //     icon.classList.add('fa-circle');
+    //     }
+});
